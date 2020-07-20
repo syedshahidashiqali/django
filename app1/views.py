d98836ddfdef8243575fd43bccf7ff485732228e
syedshahidashiqali <=>
2020-07-21 00:10:41 +0500

    changed fun1 function and greet function and added greet.html file at hello directory

--- app1/views.py
+++ app1/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def fun1(request):
-    return HttpResponse("Hello world!") #  Commented bcoz new way work start
+# def fun1(request):
+#     return HttpResponse("Hello world!") #  Commented bcoz new way work start
 
+def fun1(request):
+    return render(request, "hello/index.html")
 
 
 def fun2(request):
@@ -16,5 +18,10 @@ def fun3(request):
 def fun4(request):
     return HttpResponse("Hello Malik")
 
+# def greet(request, name):
+#     return HttpResponse(f"Hello {name.capitalize()}!")
+
 def greet(request, name):
-    return HttpResponse(f"Hello {name.capitalize()}!")
+    return render(request, "hello/greet.html", {        # 3rd argument(dict) called context,all variables that it access to is given it it
+        "name": name.capitalize()
+    })
